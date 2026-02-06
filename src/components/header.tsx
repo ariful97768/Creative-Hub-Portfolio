ee692a2857c000d6b89179a06b906548bf412912
ariful97768 <ariful97768@gmail.com>
2026-02-06 19:20:53 +0600

    Added animation to match the design on the navigation menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,35 @@
+"use client";
 import { MoveRight } from "lucide-react";
 import Image from "next/image";
 import logo from "@/assets/logo-removeBg-preview.png";
 import Link from "next/link";
 import Button from "./ui/button";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [isFloating, setIsFloating] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+
+      if (scrollTop >= 700) {
+        setIsFloating(true);
+      } else {
+        setIsFloating(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <nav className="sticky top-0 z-50 w-full bg-white">
+    <nav
+      className={
+        "sticky top-0 z-50 bg-white mx-auto transition-all duration-300 ease-in-out" +
+        (isFloating ? " rounded-full top-6 shadow-md max-w-7xl" : " max-w-full")
+      }
+    >
       <div className="max-w-360 mx-auto flex py-3 items-center justify-between px-10">
         {/* Logo */}
         <div>
