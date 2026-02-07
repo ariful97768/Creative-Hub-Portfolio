d424c3bd00fa28bfd349c5abb23a1091d84594cd
ariful97768 <ariful97768@gmail.com>
2026-02-07 17:18:07 +0600

    Made footer section responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,33 +27,33 @@ export default function ContactUsPage() {
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-          className="bg-cover bg-center h-128 w-full"
+          className="bg-cover bg-center h-40 sm:h-60 md:h-100 lg:h-128 w-full"
         ></div>
       </section>
-      <section className="max-w-360 my-20 mx-auto px-14">
-        <div className="flex rounded-xl p-6 bg-dark/90 text-white">
-          <div className="relative shrink-0 overflow-hidden flex flex-col py-11 pl-5 pr-33 bg-dark max-w-max text-white rounded-xl">
+      <section className="max-w-360 my-20 mx-auto px-5 md:px-8 lg:px-14">
+        <div className="flex flex-col-reverse lg:flex-row gap-5 rounded-xl p-0 sm:p-6 bg-dark/90 text-white">
+          <div className="relative lg:max-w-max w-full overflow-hidden flex flex-col py-11 p-5 sm:pl-5 sm:pr-14 lg:pr-33 bg-dark text-white rounded-xl">
             <div className="space-y-3 mb-20">
-              <h3 className="text-3xl font-bold">
+              <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold">
                 <span className="text-[#8FDC24]">Contact</span> Information
               </h3>
-              <p className="text-lg text-white/80">
+              <p className=" text-sm sm:text-base md:text-lg text-white/80">
                 Say something to start a live chat!
               </p>
             </div>
-            <div className="space-y-12">
-              <p className="flex items-center gap-6">
-                <PhoneCall />
+            <div className="text-sm sm:text-base space-y-5 sm:space-y-8 md:space-y-12">
+              <p className="flex items-center gap-2 sm:gap-6">
+                <PhoneCall size={24} className="shrink-0" />
                 <Link href={"tel:+08801607304149"}>+08801607304149</Link>
               </p>
-              <p className="flex items-center gap-6">
-                <Mail />
+              <p className="flex items-center gap-2 sm:gap-6">
+                <Mail size={24} className="shrink-0" />
                 <Link href={"mailto:mdshakilmirja701@gmail.com "}>
                   mdshakilmirja701@gmail.com
                 </Link>
               </p>
-              <p className="flex items-center gap-6">
-                <MapPin />
+              <p className="flex items-center gap-2 sm:gap-6">
+                <MapPin size={24} className="shrink-0" />
                 <span>Dhaka Uttara, #1201, Sector: #03</span>
               </p>
             </div>
@@ -80,9 +80,9 @@ export default function ContactUsPage() {
             <div className="bg-[#93D23A80] absolute bottom-15 right-15 w-35 h-35 rounded-full"></div>
             <div className="bg-[#93D23A80] absolute -bottom-20 -right-20 w-67 h-67 rounded-full"></div>
           </div>
-          <form className="mx-auto w-full">
+          <form className="mx-auto px-6 sm:px-0 w-full">
             <div className="flex flex-col mx-auto gap-10 pt-10 max-w-150 w-full">
-              <div className="flex gap-10">
+              <div className="flex flex-col sm:flex-row gap-10">
                 <div className="flex w-full flex-col gap-2">
                   <label className="text-sm" htmlFor="first_name">
                     First Name
@@ -102,7 +102,7 @@ export default function ContactUsPage() {
                   />
                 </div>
               </div>
-              <div className="flex gap-10">
+              <div className="flex flex-col sm:flex-row gap-10">
                 <div className="flex w-full flex-col gap-2">
                   <label htmlFor="email">Email</label>
                   <input
@@ -120,7 +120,7 @@ export default function ContactUsPage() {
                   />
                 </div>
               </div>
-              <div className="flex w-full flex-col gap-2">
+              <div className="flex flex-col w-full gap-2">
                 <label htmlFor="message">Message</label>
                 <textarea
                   name="message"
@@ -129,7 +129,7 @@ export default function ContactUsPage() {
                   placeholder="Write your message.."
                 ></textarea>
               </div>
-              <div className="flex justify-end mt-15">
+              <div className="flex justify-center sm:justify-end mb-5 lg:mb-0 lg:mt-15">
                 <button className="bg-[#8FDC24] text-white px-12 py-4 font-medium rounded-md hover:cursor-pointer active:scale-95 transition-all duration-300">
                   Send Message
                 </button>
