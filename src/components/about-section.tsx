c97f5e626274e6509d418adffaad6be2439c2f60
ariful97768 <ariful97768@gmail.com>
2026-02-07 15:26:18 +0600

    Made about section on home page responsive

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -1,16 +1,33 @@
 import Image from "next/image";
 import SectionTitle from "./ui/section-title";
-import templateImg from '@/assets/template-img-2.jpg'
+import templateImg from "@/assets/template-img-2.jpg";
 export default function AboutUsSection() {
   return (
-    <section className={'flex items-center justify-between max-w-360 mx-auto px-14'}>
-      <div className="w-122.5 h-122.5 relative">
-        <Image src={templateImg} alt="Template image" fill className="object-cover"/>
-        <div className="absolute top-1/3 -right-1/3 font-bold text-dark px-13 py-4 bg-[#E7F914] rounded-full flex flex-col items-center justify-center">
-          <h1 className="text-[56px]">7 Year's</h1>
-          <p className="text-base text-center">years of experiences<br />in this industry</p>
+    <section
+      className={
+        "flex md:flex-row flex-col-reverse sm:items-center items-start justify-between gap-10 lg:gap-30 max-w-360 mx-auto px-5 md:px-8 lg:px-14"
+      }
+    >
+      {/* Image part */}
+      <div className="shrink-0 w-60 h-60 sm:w-120 md:w-80 xl:w-122.5 sm:h-120 xl:h-122.5 relative">
+        <Image
+          src={templateImg}
+          alt="Template image"
+          fill
+          className="rounded-md shrink-0 xl:rounded-none object-cover"
+        />
+        <div className="absolute top-1/4 -right-10 lg:top-1/3 lg:-right-1/3 flex flex-col items-center justify-center font-bold text-dark px-5 py-1.5 lg:px-13 lg:py-4 bg-[#E7F914] rounded-full">
+          <h1 className="text-xl md:text-xl lg:text-5xl xl:text-[56px]">
+            7 Year's
+          </h1>
+          <p className="text-sm lg:text-base text-center">
+            years of experiences
+            <br />
+            in this industry
+          </p>
         </div>
       </div>
+      {/* Right side, text part */}
       <div className="max-w-xl">
         <div>
           <SectionTitle
@@ -18,7 +35,7 @@ export default function AboutUsSection() {
             heading="We're leading The Power Of Technology"
             subheading="ABOUT US"
           />
-          <p className="pt-3 text-dark text-base font-medium">
+          <p className="pt-3 text-dark text-sm md:text-base font-medium">
             Harnessing innovation and cutting-edge technology, we deliver smart
             solutions that empower businesses, enhance productivity, and drive
             growth. Our expertise transforms ideas into digital realities,
@@ -27,7 +44,7 @@ export default function AboutUsSection() {
           </p>
         </div>
         <div>
-          <ul className="grid grid-cols-2 pt-6 gap-x-11 gap-y-7 text-lg font-medium">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 pt-6 gap-x-5 md:gap-x-11 gap-y-3 md:gap-y-7 text-lg font-medium">
             <li>✔ Best IT Solutions & Service</li>
             <li>✔ Always Latest Technology</li>
             <li>✔ 24 Hour's Customer Service</li>
