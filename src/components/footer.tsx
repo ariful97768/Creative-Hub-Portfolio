7250a4d0f75c99f8fc6b11fd7213591c685ae411
ariful97768 <ariful97768@gmail.com>
2026-02-07 15:30:27 +0600

    Made footer section responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-[#131A2A] text-white">
-      <div className="pt-8 pb-3 max-w-360 px-14 mx-auto">
-        <div className="flex justify-between font-bold font-(family-name:--font-orbitron)">
-          <h3 className="text-center">
+      <div className="pt-8 pb-3 max-w-360 px-5 md:px-8 lg:px-14 mx-auto">
+        <div className="flex flex-col md:flex-row gap-y-10 items-center justify-between font-bold font-(family-name:--font-orbitron)">
+          <h3 className="text-center text-xl">
             Creative Hub <br /> IT Agency
           </h3>
           <div className="flex items-center gap-3">
@@ -26,9 +26,9 @@ export default function Footer() {
           </div>
         </div>
         <hr className="text-[#DDDDDD4D] mt-5.5 mb-2" />
-        <div className="flex items-center justify-between">
-          <span>Creative Hub IT Agency 2023, All Rights Reserved</span>
-          <ul className="flex items-center gap-3">
+        <div className="flex flex-col md:flex-row gap-y-10 items-center justify-between">
+          <span className="text-sm text-center md:text-left">Creative Hub IT Agency 2023, All Rights Reserved</span>
+          <ul className="flex text-xs md:text-base items-center gap-3">
             <Link href={"#"}>Terms & Condition</Link>
             <Link href={"#"}>Privacy</Link>
             <Link href={"#"}>Contact Us</Link>
