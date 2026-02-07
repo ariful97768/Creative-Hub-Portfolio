498d8362da16c7c4914f78dc1600aa15aa1be0c7
ariful97768 <ariful97768@gmail.com>
2026-02-07 12:17:07 +0600

    Made navbar responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MoveRight } from "lucide-react";
+import { Hamburger, Menu, MoveRight } from "lucide-react";
 import Image from "next/image";
 import logo from "@/assets/logo-removeBg-preview.png";
 import Link from "next/link";
@@ -26,20 +26,21 @@ export default function Header() {
   return (
     <nav
       className={
-        "sticky top-0 z-50 bg-white mx-auto transition-all duration-300 ease-in-out" +
+        "sticky top-0 z-50 bg-white text-dark mx-auto transition-all duration-300 ease-in-out" +
         (isFloating ? " rounded-full top-6 shadow-md max-w-7xl" : " max-w-full")
       }
     >
-      <div className="max-w-360 mx-auto flex py-3 items-center justify-between px-10">
+      <div className="max-w-360 mx-auto flex py-3 px-5 md:px-10 items-center justify-between">
         {/* Logo */}
-        <div>
+        <div className="relative lg:w-24 lg:h-17 w-18 h-14">
           <Link href={"/"}>
-            <Image src={logo} width={97} height={68} alt="Logo" />
+            <Image src={logo} fill className="object-contain" alt="Logo" />
           </Link>
         </div>
-        {/* Navigation */}
+
+        {/* Navigation Menus*/}
         <div>
-          <ul className="flex items-center gap-8 text-xl font-semibold">
+          <ul className="items-center lg:gap-8 md:flex hidden gap-4 lg:text-xl text-base font-semibold">
             <li>
               <Link className="hover:text-primary" href="/">
                 Home
@@ -72,14 +73,68 @@ export default function Header() {
             </li>
           </ul>
         </div>
-        {/* Contact Button */}
+
         <div className="flex items-center">
+          {/* Contact Button */}
           <Link href={"/contact"}>
             <Button variant={"primary"}>
               Get A Quote
               <MoveRight />
             </Button>
           </Link>
+
+          {/* Hamburger Icon */}
+          <div className="ml-5 block md:hidden">
+            <Menu
+              className="text-dark"
+              onClick={() => setIsOpen(!isOpen)}
+              size={28}
+            />
+          </div>
+
+          {/* Mobile menu */}
+          {isOpen && (
+            <div className="lg:hidden absolute top-20 md:top-20 right-0 w-xs border-t shadow-lg bg-white border rounded-xl mr-5 md:mr-10 border-gray-200 pb-6">
+              <div className="p-4 gap-2 max-w-xs flex flex-col">
+                <Link
+                  href="#/"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/about"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  About
+                </Link>
+                <Link
+                  href="#services"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  Services
+                </Link>
+                <Link
+                  href="/projects"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  Client Projects
+                </Link>
+                <Link
+                  href="/team"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  Our Team
+                </Link>
+                <Link
+                  href="/contact"
+                  className="block border bg-white border-gray-200 px-3 py-2 text-base font-medium text-dark hover:text-navy hover:bg-light-gray rounded-md"
+                >
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,9 +9,9 @@ export default function Button({
 }) {
   const classNames = {
     primary:
-      "flex items-center gap-2 px-4.5 py-4 rounded-lg font-bold bg-accent text-white",
+      "flex items-center gap-2 text-sm lg:text-base lg:px-4.5 lg:py-4 px-3.5 py-2.5 rounded-lg font-bold bg-accent text-white",
     secondary:
-      "flex items-center gap-2 px-4.5 py-4 rounded-lg font-bold bg-white text-[#6D6565]",
+      "flex items-center gap-2 text-sm lg:text-base lg:px-4.5 lg:py-4 px-3.5 py-2.5 rounded-lg font-bold bg-white text-[#6D6565]",
     rounded:
       "flex items-center gap-2 px-10 py-3.5 rounded-full font-bold bg-accent text-white",
     action:
@@ -19,7 +19,7 @@ export default function Button({
   };
   return (
     <button
-      className={`${classNames[variant]}  group hover:cursor-pointer active:scale-97 transition duration-300`}
+      className={`${classNames[variant]} group hover:cursor-pointer active:scale-97 transition duration-300`}
     >
       {children}
     </button>
