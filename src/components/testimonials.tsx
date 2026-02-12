f707614a76cb53ea573dd68981c850ea8b058866
ariful97768 <ariful97768@gmail.com>
2026-02-12 23:14:46 +0600

    Installed react marquee and added marquee effect on testimonials section

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,6 +1,7 @@
 import SectionTitle from "@/components/ui/section-title";
 import TestimonialCard from "@/components/testimonial-card";
 import getDb from "@/lib/db";
+import Marquee from "react-fast-marquee";
 export default async function Testimonials() {
   const db = await getDb();
   const testimonials = await db.testimonials
@@ -9,7 +10,7 @@ export default async function Testimonials() {
     .sort({ createdAt: -1 })
     .toArray();
   return (
-    <section className="space-y-12.5 max-w-360 mx-auto pb-20 px-5 md:px-8 lg:px-14">
+    <section className="space-y-10 max-w-360 mx-auto pb-20 px-5 md:px-8 lg:px-14">
       <div className="max-w-120">
         <SectionTitle
           align="left"
@@ -17,18 +18,26 @@ export default async function Testimonials() {
           subheading="Testimonial"
         />
       </div>
-      <div className="flex flex-col md:flex-row gap-3 lg:gap-6 max-w-max mx-auto">
-        {testimonials.map((testimonial) => (
-          <TestimonialCard
-            key={testimonial._id.toString()}
-            review={testimonial.review}
-            image={testimonial.image}
-            role={testimonial.position}
-            country={testimonial.country}
-            company={testimonial.company}
-          />
-        ))}
-      </div>
+      <Marquee
+        speed={25}
+        gradient
+        pauseOnHover
+        direction="left"
+        className="flex gap-3 lg:gap-6"
+      >
+        <div className="flex gap-3 py-5 lg:gap-6 max-w-max mx-auto">
+          {testimonials.map((testimonial) => (
+            <TestimonialCard
+              key={testimonial._id.toString()}
+              review={testimonial.review}
+              image={testimonial.image}
+              role={testimonial.position}
+              country={testimonial.country}
+              company={testimonial.company}
+            />
+          ))}
+        </div>
+      </Marquee>
     </section>
   );
 }
