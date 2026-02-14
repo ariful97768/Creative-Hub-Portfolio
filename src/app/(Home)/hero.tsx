ea3f0f7522c7ba09f81716d04c2293c6f9f367b6
ariful97768 <ariful97768@gmail.com>
2026-02-14 18:16:49 +0600

    added image for mobile view on hero section

--- src/app/(Home)/hero.tsx
+++ src/app/(Home)/hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import heroImg from "@/assets/hero-banner.png";
+import heroImg from "@/assets/hero-banner-copy.png";
 import { MoveRight } from "lucide-react";
 import Button from "@/components/ui/button";
 import Link from "next/link";
@@ -8,8 +8,8 @@ import WaveText from "../../components/ui/text";
 
 export default function Hero() {
   return (
-    <motion.section className="flex items-center bg-linear-to-b from-bgGradient to-[#C3E5FE57]/34 w-full">
-      <div className="relative w-full py-12 sm:py-10 max-w-360 mx-auto flex justify-end">
+    <motion.section className="relative flex items-center bg-linear-to-b from-bgGradient to-[#C3E5FE57]/34 w-full">
+      <div className="relative w-full sm:py-10 max-w-360 mx-auto flex flex-col-reverse sm:flex-row justify-end">
         <motion.div className="text-center pb-5 sm:text-start z-1 sm:absolute top-1/2 left-0 sm:-translate-y-1/2 space-y-8 px-5 md:px-8 lg:px-14 sm:w-5/7 md:w-2/3">
           <div>
             <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-primary">
@@ -25,11 +25,11 @@ export default function Hero() {
                 type: "spring",
                 stiffness: 100,
               }}
-              className="text-sm md:text-base lg:text-lg font-medium max-w-xl leading-7"
+              className="text-sm md:text-base lg:text-lg font-medium max-w-xl leading-6 sm:leading-7"
             >
               We help businesses grow with modern websites, Web3 solutions,
-              mobile apps, and result-driven UI/UX design. From idea to launch —
-              we build digital products that perform.
+              mobile apps, and result-driven UI/UX design. From idea to launch,
+              We build digital products that perform.
             </motion.p>
           </div>
           <div className="flex flex-col sm:flex-row justify-center sm:justify-start items-center gap-4 sm:gap-6">
@@ -79,9 +79,13 @@ export default function Hero() {
           initial={{ x: 40 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.7 }}
-          className="sm:w-3/5 hidden sm:block"
+          className="sm:w-3/5 px-5 md:px-0"
         >
-          <Image src={heroImg} alt="Hero Image" />
+          <Image
+            className="w-full pt-5 lg:p-10 h-full object-contain"
+            src={heroImg}
+            alt="Hero Image"
+          />
         </motion.div>
       </div>
     </motion.section>
